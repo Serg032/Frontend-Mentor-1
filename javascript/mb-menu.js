8f088ac33e5c359b28429a91343c398a1b9491c3
Sergio Radigales <sergioradigales@gmail.com>
2022-04-23 21:00:24 +0200

    more fixes

--- javascript/mb-menu.js
+++ javascript/mb-menu.js
@@ -3,12 +3,25 @@ window.addEventListener("load", () => {
   const bars = document.getElementById("bars");
   const cross = document.getElementById("cross");
 
+  const featuresDisplay = document.getElementById("featu-dis");
+  const compaDisplay = document.getElementById("comp-dis");
+  const up = document.getElementById("up");
+  const down = document.getElementById("down");
+  const compaDown = document.getElementById("down-comp");
+  const compaUp = document.getElementById("up-comp");
+
   const showMenu = () => {
     menu.style.transform = "translate(0)";
   };
 
   const hideMenu = () => {
     menu.style.transform = "translate(-100%)";
+    compaDisplay.style.display = "none";
+    featuresDisplay.style.display = "none";
+    up.style.display = "none";
+    down.style.display = "block";
+    compaDown.style.display = "block";
+    compaUp.style.display = "none";
   };
 
   bars.addEventListener("click", showMenu);
